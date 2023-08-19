35e794dd5ac7e76b1f3ff339b2296bbe7aae07bf
74Per <percent741017@gmail.com>
2023-08-19 10:57:07 +0900

    :package:

--- note.py
+++ note.py
@@ -1,10 +1,14 @@
 noteList = [
-    {'pos': 1, 'timeTick': 0.4}, 
     {'pos': 2, 'timeTick': 0.4}, 
     {'pos': 3, 'timeTick': 0.4}, 
     {'pos': 4, 'timeTick': 0.4},
-    {'pos': 1, 'timeTick': 0.6},
-    {'pos': 2, 'timeTick': 0.6},
-    {'pos': 3, 'timeTick': 0.6},
-    {'pos': 4, 'timeTick': 0.6}
+    {'pos': 1, 'timeTick': 0.4}, 
+    {'pos': 1, 'timeTick': 2},
+    {'pos': 2, 'timeTick': 2},
+    {'pos': 3, 'timeTick': 2},
+    {'pos': 4, 'timeTick': 2}, 
+    {'pos': 1, 'timeTick': 3},
+    {'pos': 2, 'timeTick': 3},
+    {'pos': 3, 'timeTick': 3},
+    {'pos': 4, 'timeTick': 3},
 ]
