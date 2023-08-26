83fc7a3ee4f3306acfdc14c985573069eec9748d
74Per <percent741017@gmail.com>
2023-08-26 12:19:52 +0900

    :construction:

--- note.py
+++ note.py
@@ -1,6 +1,12 @@
-noteList = [
+noteList = {'ETA' : [
     {'pos': 2, 'timeTick': 0.4}, 
     {'pos': 3, 'timeTick': 0.4}, 
     {'pos': 4, 'timeTick': 0.4},
     {'pos': 1, 'timeTick': 0.4},
-]
+],
+'Newjeans' : [
+    {'pos': 2, 'timeTick': 0.4}, 
+    {'pos': 3, 'timeTick': 0.4}, 
+    {'pos': 4, 'timeTick': 0.4},
+    {'pos': 1, 'timeTick': 0.4},
+]}
